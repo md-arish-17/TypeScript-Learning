70507ba64d6a01f980d3142598739fc2a9e2fa9f
md-arish-17 <it21.mdarish@svceindore.ac.in>
2026-01-06 12:32:47 +0530

    object data type and special data type

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
-var userName = "Mohammad Arish";
-var age = 21;
-var college = "Swami Vivekanand College of Engineering";
-console.log(userName);
-console.log(age);
-console.log(college);
-var arr = [];
-arr.push(20);
-console.log(arr);
+var num = 10;
+var str = "arish";
+var empty = null;
+var undefine = undefined;
+var numArray = [10, 20, 30];
+var nameArray = ["usman", "zaheen", "baji"];
+var nameArray2 = ["anas", "arish", "mammi", "papa"];
+var comArray = ["arish", 21];
+var obj = { userName: "zaheen dolly", age: 2 };
+var data = 10;
+console.log(data);
+data = "usman boss";
+console.log(data);
+var input = "Hello Baji Boss";
+if (typeof input === "string") {
+    console.log(input.toUpperCase());
+}
+function logMessage() {
+    console.log("This function does'nt return nothing");
+}
+logMessage();
+function funcError() {
+    throw new Error("Usman Toys Error");
+}
+// funcError()
+console.log(num);
+console.log(str);
+console.log(empty);
+console.log(undefine);
+console.log(numArray);
+console.log(nameArray);
+console.log(nameArray2);
+console.log(comArray);
+console.log(obj);
--- app.ts
+++ app.ts
@@ -1,11 +1,39 @@
-var userName:string = "Mohammad Arish"
-var age:number = 21
-var college:string = "Swami Vivekanand College of Engineering"
-console.log(userName)
-console.log(age)
-console.log(college)
+let num:number = 10
+let str:string = "arish"
+let empty:null = null
+let undefine:undefined = undefined
+let numArray:number[] = [10,20,30]
+let nameArray:string[] = ["usman","zaheen","baji"]
+let nameArray2:Array<string> = ["anas","arish","mammi","papa"]
+let comArray:[string,number] = ["arish",21]
+let obj:{userName:string,age:number} = {userName: "zaheen dolly",age:2}
+let data:any = 10
+console.log(data)
+data = "usman boss"
+console.log(data);
 
-let arr:number[] = []
-arr.push(20)
-console.log(arr)
+let input:unknown = "Hello Baji Boss"
+if(typeof input === "string"){
+    console.log(input.toUpperCase())
+}
+
+function logMessage():void{
+    console.log("This function does'nt return nothing")
+}
+logMessage()
+
+function funcError():never{
+    throw new Error("Usman Toys Error")
+}
+// funcError()
+
+console.log(num)
+console.log(str)
+console.log(empty)
+console.log(undefine);
+console.log(numArray)
+console.log(nameArray);
+console.log(nameArray2);
+console.log(comArray);
+console.log(obj);
 
