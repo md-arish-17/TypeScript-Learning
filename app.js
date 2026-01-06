a06dee80591a8aa7e9bf4d0eb2643c693d506e4b
md-arish-17 <it21.mdarish@svceindore.ac.in>
2026-01-06 11:17:37 +0530

    Typescript Basics

---
+++ app.js
@@ -0,0 +1,9 @@
+var userName = "Mohammad Arish";
+var age = 21;
+var college = "Swami Vivekanand College of Engineering";
+console.log(userName);
+console.log(age);
+console.log(college);
+var arr = [];
+arr.push(20);
+console.log(arr);
---
+++ app.ts
@@ -0,0 +1,11 @@
+var userName:string = "Mohammad Arish"
+var age:number = 21
+var college:string = "Swami Vivekanand College of Engineering"
+console.log(userName)
+console.log(age)
+console.log(college)
+
+let arr:number[] = []
+arr.push(20)
+console.log(arr)
+
